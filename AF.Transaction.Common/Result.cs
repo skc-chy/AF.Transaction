7a7b88cf169cfdb6f62cc3bff6b730acb401da02
Sukanta Choudhury <skc.chy@gmail.com>
2016-08-27 17:56:47 +0530

    Add files via upload

---
+++ AF.Transaction.Common/Result.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace AF.Transaction.Common
+{
+
+    [DataContract]
+    public class Result
+    {
+        [DataMember]
+        public bool IsValid { get; set; }
+        [DataMember]
+        public IList<string> Message { get; set; }
+    }
+}
