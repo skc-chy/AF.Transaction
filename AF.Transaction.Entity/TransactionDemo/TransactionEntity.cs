83d7106558dc3d588376129016ed23b48b2cb7b9
Sukanta Choudhury <skc.chy@gmail.com>
2016-08-27 17:57:17 +0530

    Add files via upload

---
+++ AF.Transaction.Entity/TransactionDemo/TransactionEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AF.Transaction.Entity.TransactionDemo
+{
+    public sealed class TransactionEntity
+    {
+        public Guid EmpID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string EMail { get; set; }
+
+        public string Phone { get; set; }
+
+    }
+}
---
+++ AF.Transaction.Entity/WCFTransactionDemo/WCFTransactionEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace AF.Transaction.Entity.WCFTransactionDemo
+{
+    [DataContract]
+    public sealed class WCFTransactionEntity
+    {
+        [DataMember]
+        public Guid EmpID { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string Address { get; set; }
+
+        [DataMember]
+        public string EMail { get; set; }
+
+        [DataMember]
+        public string Phone { get; set; }
+
+    }
+}
